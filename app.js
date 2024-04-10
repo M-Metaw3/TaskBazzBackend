212d49eac0e420e2d1f0074a10e656bcff418818
M-Metaw3 <mahmoudmetawea161@gmail.com>
2024-04-11 01:21:45 +0200

    add multer for uploading file

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const morgan = require('morgan');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorcontroller/errorController');
 // const tourRouter = require('./routes/tourRoutes');
-// const userRouter = require('./routes/userRoutes');
+const userRouter = require('./routes/test');
 
 const app = express();
-
+const handleMulterErrors = require('./controllers/errorcontroller/multerErrors');
 // 1) MIDDLEWARES
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
@@ -18,18 +18,19 @@ app.use(express.json());
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
+  console.log(req.requestTime)
   next();
 });
 
 // 3) ROUTES
-app.use('/api/v1/tours', ()=>{
-  console.log('Tour router')});
-// app.use('/api/v1/users', userRouter);
+// app.use('/api/v1/tours', ()=>{
+//   console.log('Tour router')});
+app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
+app.use(handleMulterErrors);
 app.use(globalErrorHandler);
-
 module.exports = app;
---
+++ routes/test.js
@@ -0,0 +1,21 @@
+const  express = require('express');
+const router = express.Router();
+const createMulterUpload= require('../utils/multer');
+
+
+// Create multer upload middleware with custom file path
+const productImageUpload = createMulterUpload('images/products/');
+
+// Define the route for uploading product images
+router.post('/', productImageUpload.single('productImage'), (req, res) => {
+    console.log(req.file)
+  if (!req.file) {
+    return res.status(400).send('No file uploaded.');
+  }
+
+  // Handle the uploaded file here (e.g., save file path to database, return response, etc.)
+  const filePath = req.file.path;
+  res.status(200).json({ success: true, filePath });
+});
+
+module.exports = router;
